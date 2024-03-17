04f10d58fe0ac3c7097934578cf3cff889573013
victors3136 <victor.selegean@stud.ubbcluj.ro>
2024-03-17 12:19:50 +0200

    Some nicer logging along the way

--- DataValidation/InputSanitizer.cs
+++ DataValidation/InputSanitizer.cs
@@ -8,12 +8,12 @@ namespace ModerationProject.DataValidation
     }
     public class SQLValidator : IValidator
     {
-        private static readonly Regex tautologyRegex = new("(.*)\\s*=\\s*\\1", RegexOptions.None);
+        private static readonly Regex tautologyRegex = new("\\b(.*)\\s*=\\s*\\1\\b", RegexOptions.None);
         static private string CheckForBatched(string toValidate) 
         {
             if (toValidate.Contains(';'))
             {
-                Console.WriteLine("Potential SQL attack spotted ", toValidate);
+                Console.WriteLine("Spotted \";\"");
                 toValidate = toValidate.Split(';')[0];
             }
             return toValidate;
@@ -23,14 +23,19 @@ namespace ModerationProject.DataValidation
             Match checkForTautology = tautologyRegex.Match(toValidate);
             if (checkForTautology.Success)
             {
-                Console.WriteLine("Found {0} at position {1}", checkForTautology.Value, checkForTautology.Index);
+                Console.WriteLine("Spotted \"{0}\"", checkForTautology.Value);
                 toValidate = toValidate.Replace(checkForTautology.Value, "");
             }
             return toValidate;
         }
         public string Apply(string toValidate)
-        {   
-            return CheckForTautology(CheckForBatched(toValidate));
+        {
+            Console.WriteLine("Input: {0}", toValidate);
+            toValidate = CheckForBatched(toValidate);
+            Console.WriteLine("After step 1: {0}", toValidate);
+            toValidate = CheckForTautology(toValidate);
+            Console.WriteLine("After step 2: {0}", toValidate);
+            return toValidate;
         }
     }
     public class CensoringValidator : IValidator
